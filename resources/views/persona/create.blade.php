8ee6cfeec75f823b07669d9d72ba68476811be69
Ammy Izcano <ammyizca@gmail.com>
2023-03-02 11:08:07 -0600

    logo

--- resources/views/persona/create.blade.php
+++ resources/views/persona/create.blade.php
@@ -25,7 +25,7 @@
         
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-between mb-4 border-bottom">
                 <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                <img src="https://cdn.icon-icons.com/icons2/2489/PNG/96/vaccination_icon_150143.png" alt="logo" width="60px" height="60px">
+                <img src="https://cdn-icons-png.flaticon.com/128/9842/9842056.png" alt="logo" width="60px" height="60px">
             </a>
 
             <!-- Modal -->
